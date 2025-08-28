44d0e79870c6bfa262f68be8341ad9c61e3cf612
theherzanatics <zaheralunaquiatan@gmail.com>
2025-08-28 08:07:54 +0800

    Add tailwind.config.js 1756339672188

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        darkbg: '#1e293b', // dark blue-gray background for dark mode
+        darktext: '#cbd5e1', // light gray text for dark mode
+        upblue: '#1e40af',
+        upgold: '#f59e0b',
+        ateneoblue: '#003366',
+        ateneogold: '#ffcc00',
+        dlsugreen: '#007a33',
+        dlsugold: '#a7a8aa',
+      },
+    },
+  },
+  plugins: [],
+}
